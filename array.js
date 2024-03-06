730811bf390aded1abcdffe14990b59f4233d292
Shariq Choudhary <145042629+0xshariq@users.noreply.github.com>
2024-03-06 09:03:55 +0000

    H

---
+++ array.js
@@ -0,0 +1,9 @@
+// array destructuring
+
+const myArray = ["value1","value2","value3","value4","value5"];
+const myNewArray = [];
+let [Val1,Val2,Val3, ...myNewArray];
+console.log(myNewArray);
+console.log(Val1);
+console.log(Val2);
+console.log(Val3);
---
+++ object.js
@@ -0,0 +1,21 @@
+// object
+
+const person = {
+  name : "Sharique",
+  age : 22,
+  gender : "Male",
+  hobbies : ["Gaming","Coding"],
+  address : {
+    district : "Thane",
+    city : "Mumbai",
+    country : "India"
+  }
+}
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.gender);
+console.log(person.hobbies[0],person.hobbies[1]);
+console.log(person.address.city);
+console.log(person.address.country);
+console.log(person.address.district);
