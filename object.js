c17d1fd3612b6f5f508db8a6ad818766f193eaf9
Shariq Choudhary <145042629+0xshariq@users.noreply.github.com>
2024-03-07 04:15:27 +0000

    H

--- object.js
+++ object.js
@@ -14,6 +14,8 @@ const person = {
 }
 // another method to print object key value
 console.log(person["name"]);
+
+
 console.log(person.name);
 console.log(person.age);
 console.log(person.gender);
@@ -23,9 +25,13 @@ console.log(person.hobbies[0],person.hobbies[1]);
 console.log(person.address.city);
 console.log(person.address.country);
 console.log(person.address.district);
+
+// adding in object using dot notation
 person.address.building = "Palace";
+
 console.log(person.address.building);
 
-
+// adding in object using bracket notation
 person[key] = "ztejd@example.com";
+
 console.log(person[key]);
