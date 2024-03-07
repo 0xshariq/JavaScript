b8f2d362d10d32f4315aa1758adb9f1944ec8b49
Shariq Choudhary <145042629+0xshariq@users.noreply.github.com>
2024-03-07 04:22:02 +0000

    H

--- array.js
+++ array.js
@@ -2,7 +2,9 @@
 
 const myArray = ["value1","value2","value3","value4","value5"];
 const myNewArray = [];
-let [Val1,Val2,Val3, ...myNewArray];
+// destructuring assignment
+let [Val1,Val2,Val3] = myArray;
+myNewArray.push("value4","value5");
 console.log(myNewArray);
 console.log(Val1);
 console.log(Val2);
