dfe78a2f9610fbb6d2917559ac9d0b2d9178dadb
Shariq Choudhary <145042629+0xshariq@users.noreply.github.com>
2024-03-09 11:11:44 +0000

    H

--- array.js
+++ array.js
@@ -84,4 +84,87 @@ console.log(Price);
 
 
 // sort method
-// 
+// it modified the original array
+// it sorts the array in ascending order
+// it uses ASCII value for sorting
+
+// prints an unexpected output
+const numbers = [400,800,1900,610,230];
+numbers.sort();
+console.log(numbers);
+
+
+const string = ["Batman","ABC","superman","Deadpool","hawkeye"];
+string.sort();
+console.log(string);
+
+// prints an expected output
+// sorts the array in ascending order
+const numbers = [400,800,1900,610,230];
+numbers.sort((a,b) => {
+  return a - b;
+});
+console.log(numbers);
+
+// for descending order
+const numbers = [400,800,1900,610,230];
+numbers.sort((a,b) => {
+  return b - a;
+});
+console.log(numbers);
+
+// low to high
+const userCart = [
+  {productId : 1, productName : "Mobile", price : 12000},
+  {productId : 2, productName : "Laptop", price : 22000},
+  {productId : 3, productName : "Macbook", price : 250000},
+];
+userCart.sort((a,b) => {
+  return a.price - b.price;
+});
+console.log(userCart);
+
+// high to low
+const userCart = [
+  {productId : 1, productName : "Mobile", price : 12000},
+  {productId : 2, productName : "Laptop", price : 22000},
+  {productId : 3, productName : "Macbook", price : 250000},
+];
+userCart.sort((a,b) => {
+  return b.price - a.price;
+});
+console.log(userCart);
+
+
+// find method
+// it returns a single value
+// prints only the first occurence if true
+const numbers = [1,8,9,5,2];
+const even = numbers.find((number)=>{
+  return number%2 === 0;
+});
+console.log(even);
+
+
+const myArray = ["hello","cat","dog","building"];
+const value = myArray.find((val) => {
+  return val.length < 3;
+});
+console.log(value);
+
+
+// every method
+// it returns a boolean value
+// it checks if all the elements in the array satisfies the condition or not
+const numbers = [1,3,7,8,2];
+const ans = numbers.every((number) => number % 2 === 0);
+console.log(ans);
+
+const userCart = [
+  {productId : 1, productName : "Mobile", price : 12000},
+  {productId : 2, productName : "Laptop", price : 22000},
+  {productId : 3, productName : "Macbook", price : 250000},
+];
+const ans = userCart.every((user) => user.price < 30000);
+console.log(ans)
+  ;
