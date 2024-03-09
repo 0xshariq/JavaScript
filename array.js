4e61374fbe422cf57a3223b24b1008df4e29e40a
Shariq Choudhary <145042629+0xshariq@users.noreply.github.com>
2024-03-09 11:40:28 +0000

    H

--- array.js
+++ array.js
@@ -30,6 +30,7 @@ const detail = users.forEach((user) =>{                  console.log(user.userId
 console.log(detail);
 
 
+
 // map method
 // it returns a new array
 const numbers = [4,8,9,6,2];
@@ -50,11 +51,12 @@ const username = users.map((user) => {
 console.log(username);
 
 
+
 // filter method
 // it returns a new array
 const numbers = [4,8,9,6,2];
 const even = numbers.filter((number) => {
-  return number % 2 === 0;
+      return number % 2 === 0;
 });
 console.log(even);
 
@@ -62,6 +64,7 @@ const odd = numbers.filter((number) => {
   return number % 2 !== 0;
 });
 console.log(odd);
+
 
 
 // reduce method
@@ -83,6 +86,7 @@ const Price = userCart.reduce((totalPrice,currentPrice) => {
 console.log(Price);
 
 
+
 // sort method
 // it modified the original array
 // it sorts the array in ascending order
@@ -92,7 +96,6 @@ console.log(Price);
 const numbers = [400,800,1900,610,230];
 numbers.sort();
 console.log(numbers);
-
 
 const string = ["Batman","ABC","superman","Deadpool","hawkeye"];
 string.sort();
@@ -136,6 +139,7 @@ userCart.sort((a,b) => {
 console.log(userCart);
 
 
+
 // find method
 // it returns a single value
 // prints only the first occurence if true
@@ -145,12 +149,12 @@ const even = numbers.find((number)=>{
 });
 console.log(even);
 
-
 const myArray = ["hello","cat","dog","building"];
 const value = myArray.find((val) => {
   return val.length < 3;
 });
 console.log(value);
+
 
 
 // every method
@@ -166,5 +170,54 @@ const userCart = [
   {productId : 3, productName : "Macbook", price : 250000},
 ];
 const ans = userCart.every((user) => user.price < 30000);
-console.log(ans)
-  ;
+console.log(ans);
+
+
+
+// some method
+// it returns a boolean value
+// it checks if any of the elements in the array satisfies the condition or not
+const numbers = [1,3,7,8,2];
+const ans = numbers.some((number) => number % 2 === 0);
+console.log(ans);
+
+const userCart = [
+  {productId : 1, productName : "Mobile", price : 12000},
+  {productId : 2, productName : "Laptop", price : 22000},
+  {productId : 3, productName : "Macbook", price : 250000},
+];
+const ans = userCart.some((user) => user.price > 100000);
+console.log(ans);
+
+
+
+// includes method
+// it returns a boolean value
+// it checks if the element is present in the array or not
+const numbers = [1,3,7,8,2];
+const ans = numbers.includes((number) => number % 2 === 0);
+console.log(ans);
+
+
+
+// fill method
+// value , start , end (not included)
+// it returns a new array
+// it fills the array with the value
+const myArray = new Array(10).fill(-1);
+console.log(myArray);
+
+const array = [1,2,3,4,5,6,7,8,9];
+const newArray = array.fill(0,2,5);
+console.log(newArray);
+
+
+
+// splice method
+// start , delete (index number), insert
+// it modifies the original array
+// it deletes and insert the elements in the new array
+// it returns the deleted elements in a new array
+const myArray = ["item1","item2","item3"];
+myArray.splice(0,2,"inserted item4","inserted item5");
+console.log(myArray);
